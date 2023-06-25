cb2146e5d6a72359f3f112b164e4fedd15b49594
Conner Kim <dudrjs145@naver.com>
2023-06-25 20:30:45 +0900

    Initial Commit

---
+++ PlayVideo/PlayVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayVideoApp.swift
+//  PlayVideo
+//
+//  Created by Conner on 2023/06/25.
+//
+
+import SwiftUI
+
+@main
+struct PlayVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
